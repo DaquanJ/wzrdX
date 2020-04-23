dc033e6057c7f71935bc0c83e28c1b521bf13d38
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-23 12:35:50 -0400

    changing app.js to a class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
 
+      </div>
+     );
+  }
+}
+ 
 export default App;
