24be8d7d4c455bcb71c23720a945c30b1a38929b
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-24 16:15:07 -0400

    establishing dynamic rendering of more data to category page via link

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Categories = (props) => {
     return ( 
-        <div>
-            
-            {/* <h1> {props.character} </h1> */}
+        <div >
+            <Link to={`/category/${props.character}`} >
+            <img src={props.image} alt=""/>
+            <h1 > {props.character} </h1>
+            </Link>
         </div>
      );
 }
