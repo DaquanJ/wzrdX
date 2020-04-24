5bc37247a802ad022b1974b8628a48d1672188d5
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-24 11:59:41 -0400

    establishing category component

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Category extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1> Category </h1>
+            </div>
+         );
+    }
+}
+ 
+export default Category;
