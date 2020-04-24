42551205822b8134f564b896f69fa09af9b7b57f
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-23 23:26:29 -0400

    all elements added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return ( 
-        <div>
-            
+        <div className='footer' >
+            <hr/>
+            <ul>
+                <li> About </li>
+                <li> &copy; 2020 WZRDX FROM PERSCHOLAS </li>
+            </ul>
         </div>
      );
 }
