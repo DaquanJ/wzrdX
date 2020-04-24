45d5cacc6d2a67909cf8049cc66e218d82a729c2
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-24 15:10:02 -0400

    established dynamic rendering for characters to category page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Browse = (props) => {
     return ( 
         <div className='browse-cards' >
-            <Link className='browse-card' to={`/category${props.index}`} >
+            <Link className='browse-card' to={`/category/${props.name}`} >
             <img src={props.image} alt="Wizards and Witches"/>
             <h1> {props.name} </h1>
             </Link>
