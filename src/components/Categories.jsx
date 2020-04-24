d54e09e1faa473a67dc445209cfc17f24b9dbe2f
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-23 22:02:21 -0400

    establishing categories component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Categories = (props) => {
+    return ( 
+        <div>
+            
+            {/* <h1> {props.character} </h1> */}
+        </div>
+     );
+}
+ 
+export default Categories;
