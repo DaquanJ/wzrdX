f7d0642e94bfca32860c1962efc45698dce686f3
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-24 21:15:43 -0400

    added alt tag to img element

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,7 +5,7 @@ const Categories = (props) => {
     return ( 
         <div className='categories'>
             <Link id='categories-link' to={`/category/${props.character}`} >
-            <img src={props.image} alt=""/>
+            <img src={props.image} alt="characters"/>
             <h1 > {props.character} </h1>
             </Link>
         </div>
