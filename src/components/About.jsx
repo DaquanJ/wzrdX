07ca94b025188303d18922bce70fad0f4c5cfc18
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-24 21:36:42 -0400

    importing pics

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import aboutPic from '../assets/aboutpic1.jpg';
 
 const About = () => {
     return ( 
         <div>
-            <h1> About </h1>
+            <img src={aboutPic} alt="about page picture"/>
         </div>
      );
 }
