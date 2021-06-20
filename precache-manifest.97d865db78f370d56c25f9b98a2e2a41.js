45c60106bcc993fc5bf33f5fcf8817ac141ff635
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-19 20:07:29 -0400

    Updates

---
+++ precache-manifest.97d865db78f370d56c25f9b98a2e2a41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8328673d281b0742b29046848d2da2",
+    "url": "/wzrdX/index.html"
+  },
+  {
+    "revision": "a49f6d4367bf48e16c4a",
+    "url": "/wzrdX/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ad0a6a70eb7de92c405c",
+    "url": "/wzrdX/static/css/main.20d7f43c.chunk.css"
+  },
+  {
+    "revision": "a49f6d4367bf48e16c4a",
+    "url": "/wzrdX/static/js/2.49d32ea4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wzrdX/static/js/2.49d32ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0a6a70eb7de92c405c",
+    "url": "/wzrdX/static/js/main.51d631eb.chunk.js"
+  },
+  {
+    "revision": "581d7cf533048588432a",
+    "url": "/wzrdX/static/js/runtime-main.f24eaee9.js"
+  },
+  {
+    "revision": "d3ebc508f5f89e9aa7c74a4cfdf03de5",
+    "url": "/wzrdX/static/media/HP5.d3ebc508.jpeg"
+  },
+  {
+    "revision": "e755d328a1fe0b9bba3b1240e091895f",
+    "url": "/wzrdX/static/media/aboutpic2.e755d328.jpg"
+  }
+]);
